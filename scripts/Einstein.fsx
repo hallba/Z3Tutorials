50fdf1332d4bfcd61bd20111261b8fca81c18480
Ben Hall <BH418@Hutchison-MRC.cam.ac.uk>
2017-02-03 17:37:11 +0000

    Working riddle

--- scripts/Einstein.fsx
+++ scripts/Einstein.fsx
@@ -3,6 +3,11 @@
 
 open Microsoft.Z3 
 
+// From
+// https://udel.edu/~os/riddle.html
+
+let mk_distinct (z3:Context) (tt:Expr list) = 
+    z3.MkDistinct (List.toArray tt)
 
 let main () = 
     let ctx = new Context()
@@ -17,24 +22,35 @@ let main () =
     //All live at house numbers from 1 to 5
     let range item = ctx.MkAnd(ctx.MkGe(item,ctx.MkInt(1)),ctx.MkLe(item,ctx.MkInt(5)))
     let streetRange = ctx.MkAnd(Array.collect (fun char -> Array.map range char) everything)
+    //Make everything distinct
+    let distinctNat = mk_distinct ctx [nationalities.[0];nationalities.[1];nationalities.[2];nationalities.[3];nationalities.[4]]
+    let distinctCol = mk_distinct ctx [houseColours.[0];houseColours.[1];houseColours.[2];houseColours.[3];houseColours.[4]]
+    let distinctDri = mk_distinct ctx [drinks.[0];drinks.[1];drinks.[2];drinks.[3];drinks.[4]]
+    let distinctSmo = mk_distinct ctx [smokes.[0];smokes.[1];smokes.[2];smokes.[3];smokes.[4]]
+    let distinctPet = mk_distinct ctx [pets.[0];pets.[1];pets.[2];pets.[3];pets.[4]]
     //Now look at the facts (NB houses read 1 to 5 left to right)
     let britImpliesRed = ctx.MkEq(ctx.MkIntConst("Briton"),ctx.MkIntConst("Red"))
     let swedeImpliesDogs = ctx.MkEq(ctx.MkIntConst("Swede"),ctx.MkIntConst("Dogs"))
     let daneImpliesTea = ctx.MkEq(ctx.MkIntConst("Dane"),ctx.MkIntConst("Tea"))
-    let greenImpliesWhiteMinusOne = ctx.MkEq(ctx.MkIntConst("White"),ctx.MkSub(ctx.MkIntConst("Green"),ctx.MkInt(1)))
+    let greenImpliesWhiteMinusOne = ctx.MkEq(ctx.MkIntConst("White"),ctx.MkAdd(ctx.MkIntConst("Green"),ctx.MkInt(1)))
     let greenImpliesCoffee = ctx.MkEq(ctx.MkIntConst("Green"),ctx.MkIntConst("Coffee"))
     let pallmallImpliesBirds = ctx.MkEq(ctx.MkIntConst("PallMall"),ctx.MkIntConst("Birds"))
     let yellowImpliesDunhill = ctx.MkEq(ctx.MkIntConst("Yellow"),ctx.MkIntConst("Dunhill"))
     let middleImpliesMilk = ctx.MkEq(ctx.MkIntConst("Milk"),ctx.MkInt(3))
     let norwegianImpliesOne = ctx.MkEq(ctx.MkIntConst("Norwegian"),ctx.MkInt(1))
-    let blendImpliesCats = ctx.MkEq(ctx.MkIntConst("Blend"),ctx.MkIntConst("Cats"))
-    let horsesImpliesDunhill = ctx.MkEq(ctx.MkIntConst("Horses"),ctx.MkIntConst("Dunhill"))
+    let blendImpliesCatsNeighbour = ctx.MkOr(ctx.MkEq(ctx.MkIntConst("Blend"),ctx.MkAdd(ctx.MkIntConst("Cats"),ctx.MkInt(1))),ctx.MkEq(ctx.MkIntConst("Blend"),ctx.MkSub(ctx.MkIntConst("Cats"),ctx.MkInt(1))))
+    let horsesImpliesDunhillNeighbour = ctx.MkOr(ctx.MkEq(ctx.MkIntConst("Horses"),ctx.MkAdd(ctx.MkIntConst("Dunhill"),ctx.MkInt(1))),ctx.MkEq(ctx.MkIntConst("Horses"),ctx.MkSub(ctx.MkIntConst("Dunhill"),ctx.MkInt(1))))
     let bluemasterImpliesBeer = ctx.MkEq(ctx.MkIntConst("BlueMaster"),ctx.MkIntConst("Beer"))
     let germanImpliesPrince = ctx.MkEq(ctx.MkIntConst("German"),ctx.MkIntConst("Prince"))
     let norwegianImpliesBlueNeighbour = ctx.MkOr(ctx.MkEq(ctx.MkIntConst("Norwegian"),ctx.MkAdd(ctx.MkIntConst("Blue"),ctx.MkInt(1))),ctx.MkEq(ctx.MkIntConst("Norwegian"),ctx.MkSub(ctx.MkIntConst("Blue"),ctx.MkInt(1))))
     let blendImpliesWaterNeighbour = ctx.MkOr(ctx.MkEq(ctx.MkIntConst("Blend"),ctx.MkAdd(ctx.MkIntConst("Water"),ctx.MkInt(1))),ctx.MkEq(ctx.MkIntConst("Blend"),ctx.MkSub(ctx.MkIntConst("Water"),ctx.MkInt(1))))
     let s = ctx.MkSolver()
     let constraints = ctx.MkAnd( [| streetRange
+                                    distinctNat
+                                    distinctCol
+                                    distinctDri
+                                    distinctSmo
+                                    distinctPet
                                     britImpliesRed ;
                                     swedeImpliesDogs; 
                                     daneImpliesTea; 
@@ -45,22 +61,35 @@ let main () =
                                     middleImpliesMilk;
                                     norwegianImpliesOne
                                     blendImpliesCats
-                                    horsesImpliesDunhill
+                                    //horsesImpliesDunhillNeighbour
                                     bluemasterImpliesBeer
                                     germanImpliesPrince
                                     norwegianImpliesBlueNeighbour
-                                    blendImpliesWaterNeighbour |] )
+                                    // blendImpliesWaterNeighbour 
+                                    |] )
+    s.Add(constraints)
     match s.Check([||]) with
     | Status.SATISFIABLE -> 
         printf "Sat\n"
-        let number char =   Array.map (fun (item:IntExpr) -> sprintf "%O" (s.Model.ConstInterp(item)) ) char
-                            |> String.concat "\t"
-        let a = number nationalities
-        // printf "%s\n" (number nationalities)
-        // printf "%s\n" (number houseColours)
-        // printf "%s\n" (number drinks)
-        // printf "%s\n" (number smokes)
-        // printf "%s\n" (number pets)
+        let number char =   Array.map (fun (item:IntExpr) -> (int(sprintf "%O" (s.Model.ConstInterp(item))),(sprintf "%O" item)) ) char
+                            |> Map.ofArray
+        // let a = number nationalities]
+        printf "\t%-10d\t%-10d\t%-10d\t%-10d\t%-10d\n" 1 2 3 4 5
+        for i = 1 to 5 do
+            printf "%10s\t" (number nationalities).[i]
+        printf "\n"
+        for i = 1 to 5 do
+            printf "%10s\t" (number houseColours).[i]
+        printf "\n"
+        for i = 1 to 5 do
+            printf "%10s\t" (number smokes).[i]
+        printf "\n"
+        for i = 1 to 5 do
+            printf "%10s\t" (number drinks).[i]
+        printf "\n"
+        for i = 1 to 5 do
+            printf "%10s\t" (number pets).[i]
+        printf "\n"
         ()
     | Status.UNSATISFIABLE -> printf "Unsat\n"
     | _ -> failwith "Unknown response from solver"
