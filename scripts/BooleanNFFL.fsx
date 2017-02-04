67207ff38a7a059ad91a71a65ce035aea1918e27
Ben Hall <BH418@Hutchison-MRC.cam.ac.uk>
2017-02-04 19:54:29 +0000

    Completed stability proof

--- scripts/BooleanNFFL.fsx
+++ scripts/BooleanNFFL.fsx
@@ -3,6 +3,7 @@
 
 open Microsoft.Z3 
 
+//Prove stability in a negative feedforward loop (Perfect adaption motif)
 let makeVariable (ctx:Context) name time =
     ctx.MkBoolConst(sprintf "%s-%d" name time)
 
@@ -21,15 +22,70 @@ let step (ctx:Context) (s:Solver) t t' =
                                 |]  )
     s.Add(ctx.MkAnd([|input'; a'; b'; output'|]))
 
-let findFixpoint (ctx:Context) =
+let excludeState (ctx:Context) (s:Solver) t =
+    s.Add(ctx.MkNot(ctx.MkAnd(
+        [|
+        ctx.MkEq(s.Model.Eval(makeVariable ctx "Input" t),makeVariable ctx "Input" t)
+        ctx.MkEq(s.Model.Eval(makeVariable ctx "A" t),makeVariable ctx "A" t)
+        ctx.MkEq(s.Model.Eval(makeVariable ctx "B" t),makeVariable ctx "B" t)
+        ctx.MkEq(s.Model.Eval(makeVariable ctx "Output" t),makeVariable ctx "Output" t)
+        |]
+    )))
+
+type result = Bifurcation | NoBifurcation | Cycle | Stable 
+
+let findFixpoints _ =
+    let ctx = new Context()    
     let s = ctx.MkSolver()
     step ctx s 0 0
     match s.Check() with
     | Status.SATISFIABLE -> printf "Found Fixpoint\n"
                             ignore(List.map (fun name -> printf "%s:\t%O\n" name (s.Model.Eval(makeVariable ctx name 0)) ) ["Input";"A";"B";"Output"])
-    | Status.UNSATISFIABLE -> printf "No fixpoints\n"
+                            excludeState ctx s 0
+                            match s.Check() with
+                            | Status.UNSATISFIABLE -> printf "Only a single fixpoint\n"; NoBifurcation
+                            | Status.SATISFIABLE -> printf "Found bifurcation\n"; Bifurcation
+                            | _ -> failwith "Unknown result from Z3"
+    | Status.UNSATISFIABLE -> printf "No fixpoints\n"; NoBifurcation
     | _ -> failwith "Unknown result from fixpoint search"
 
-let main _ = 
+let statesEqual (ctx:Context) t t' =
+    ctx.MkAnd(
+        [|
+        ctx.MkEq(makeVariable ctx "Input" t',makeVariable ctx "Input" t)
+        ctx.MkEq(makeVariable ctx "A" t',makeVariable ctx "A" t)
+        ctx.MkEq(makeVariable ctx "B" t',makeVariable ctx "B" t)
+        ctx.MkEq(makeVariable ctx "Output" t',makeVariable ctx "Output" t)
+        |]
+    )
+
+let statesAreEqual ctx (s:Solver) t t' = 
+    s.Add(statesEqual ctx t t')
+let statesAreNotEqual (ctx:Context) (s:Solver) t t' =
+    s.Add(ctx.MkNot(statesEqual ctx t t'))
+
+let findCycles bound =
     let ctx = new Context()
-    findFixpoint ctx
+    let s = ctx.MkSolver()
+    printf "Searching for cycles at bound"
+    let rec core i bound =
+        printf "...%d" i;
+        step ctx s (i-1) i
+        s.Push()
+        //Need to assert that first state is not a fixpoint
+        statesAreNotEqual ctx s 0 1
+        statesAreEqual ctx s 0 i
+        match s.Check() with 
+        | Status.SATISFIABLE -> printf "Found cycle of length %d\n" i; Cycle
+        | Status.UNSATISFIABLE -> s.Pop(); if i < bound then core (i+1) bound else Stable
+        | _ -> failwith "Unknown result"
+    core 1 bound
+
+
+let main _ = 
+    match findFixpoints () with
+    | Bifurcation -> ()
+    | _ -> match findCycles 16 with
+            | Cycle -> ()
+            | Stable -> printf "Model is stable!\n"
+            | _ -> failwith "problem- error"
