8708f0d205d32965e7303a9ede455623a211ea12
Ben Hall <BH418@Hutchison-MRC.cam.ac.uk>
2018-10-16 11:21:08 +0100

    Now test for x64/x86

--- scripts/getZ3.fsx
+++ scripts/getZ3.fsx
@@ -10,28 +10,46 @@ module getZ3 =
     let wc = new WebClient()
 
     type OS =
-            | OSX            
-            | Windows
-            | Linux
+            | OSXx64            
+            | Windowsx64
+            | Linuxx64
+            | OSXx86           
+            | Windowsx86
+            | Linuxx86
 
     let getOS = 
-            match int Environment.OSVersion.Platform with
-            | 4 | 128 -> Linux
-            | 6       -> OSX
-            | _       -> Windows
+            match int Environment.OSVersion.Platform, Environment.Is64BitProcess with
+            | ((4 | 128),true) -> Linuxx64
+            | (6,true)       -> OSXx64
+            | (_,true)       -> Windowsx64
+            | ((4 | 128),_) -> Linuxx86
+            | (6,_)       -> OSXx86
+            | _     -> Windowsx86
 
     if  true <> System.IO.File.Exists("platform/z3/LICENSE.txt") then 
         match getOS with
-        | Linux ->  wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-16.04.zip", @"z3.zip")
-                    //This will take a while
-                    ZipFile.ExtractToDirectory("z3.zip", ".") 
-                    System.IO.Directory.Move("z3-4.6.0-x64-ubuntu-16.04","platform/z3")
-        | Windows ->wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-win.zip", @"z3.zip")
-                    //This will take a while
-                    ZipFile.ExtractToDirectory("z3.zip", ".") 
-                    System.IO.Directory.Move("z3-4.6.0-x64-win","platform/z3")
-        | OSX ->    wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-osx-10.11.6.zip", @"z3.zip")
-                    //This will take a while
-                    ZipFile.ExtractToDirectory("z3.zip", ".") 
-                    System.IO.Directory.Move("z3-4.6.0-x64-osx-10.11.6","platform/z3")
+        | Linuxx64 ->       wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-16.04.zip", @"z3.zip")
+                            //This will take a while
+                            ZipFile.ExtractToDirectory("z3.zip", ".") 
+                            System.IO.Directory.Move("z3-4.6.0-x64-ubuntu-16.04","platform/z3")
+        | Windowsx64 ->     wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-win.zip", @"z3.zip")
+                            //This will take a while
+                            ZipFile.ExtractToDirectory("z3.zip", ".") 
+                            System.IO.Directory.Move("z3-4.6.0-x64-win","platform/z3")
+        | OSXx64 ->         wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-osx-10.11.6.zip", @"z3.zip")
+                            //This will take a while
+                            ZipFile.ExtractToDirectory("z3.zip", ".") 
+                            System.IO.Directory.Move("z3-4.6.0-x64-osx-10.11.6","platform/z3")
+        | Linuxx86 ->       wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x64-ubuntu-16.04.zip", @"z3.zip")
+                            //This will take a while
+                            ZipFile.ExtractToDirectory("z3.zip", ".") 
+                            System.IO.Directory.Move("z3-4.6.0-x86-ubuntu-16.04","platform/z3")
+        | Windowsx86 ->     wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x86-win.zip", @"z3.zip")
+                            //This will take a while
+                            ZipFile.ExtractToDirectory("z3.zip", ".") 
+                            System.IO.Directory.Move("z3-4.6.0-x86-win","platform/z3")
+        | OSXx86 ->         wc.DownloadFile("https://github.com/Z3Prover/z3/releases/download/z3-4.6.0/z3-4.6.0-x86-osx-10.11.6.zip", @"z3.zip")
+                            //This will take a while
+                            ZipFile.ExtractToDirectory("z3.zip", ".") 
+                            System.IO.Directory.Move("z3-4.6.0-x86-osx-10.11.6","platform/z3")
         | _ -> ()
